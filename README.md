3b0bc7ceb892649faf012eb0537f53073291e6a9
Rafael Carvalho <rafa.aurelio6@gmail.com>
2017-05-30 04:15:29 -0300

    first

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TAKE A PET
 Take a pet visa ajudar a divulgar e promover cães, gatos e outros animais comunitários ou desabrigados através de uma maneira simples e rápida para que consigam achar um lar e uma família para amá-los.
 
-# INTALANDO O SISTEMA
+# INSTALANDO O SISTEMA
 1. Checkout no projeto.
 2. Execute o comando: `npm install`, para instalar as dependências necessárias.
 3. Execute o comando: `node server.js` para iniciar a aplicação.
--- server.js
+++ server.js
@@ -53,6 +53,10 @@ app.get("/cadastrar-usuario.html", function(request, response){
     response.render("cadastrar-usuario.ejs");
 });
 
+app.get("/cadastrarUsuario.html", function(request, response){
+    response.render("cadastrarUsuario.ejs");
+});
+
 app.post("/cadastrar-user-action", function(request, response){
     console.log(JSON.stringify(request.body));
     var newUser = {
@@ -74,9 +78,44 @@ app.post("/cadastrar-user-action", function(request, response){
 
 });
 
+app.post("/cadastrarUsuario-action", function(request, response){
+    var newUser = {
+        name: request.body.nome,
+        age: request.body.dataNascimento,
+        email: request.body.email,
+        address: request.body.endereco
+    };
+
+    try{
+        //UserDAO.validate(newUser);
+        UserDAO.save(db, newUser);
+        response.render("message.ejs", {message: "Usuário cadastrado com sucesso"});
+    }catch (err){
+        response.render("message.ejs", {message: "Erro ao cadastrar usuário: " + err});
+    }
+
+});
+
 //PET
 app.get("/cadastrar-pet.html", function(request, response){
     response.render("cadastrar-pet.ejs");
+});
+
+app.get("/cadastrarPet.html", function(request, response){
+    response.render("cadastrarPet.ejs");
+});
+
+app.get("/listarPets.html", function(request, response){
+    try{
+        let allPets = PetDAO.findAll(db, function(pets){
+            response.render("listarPets.ejs", {
+                petList: pets
+            });
+        });
+    }catch (err){
+        response.render("message.ejs", {message: "Erro ao listar pets: " + err});
+    }
+
 });
 
 app.post("/cadastrar-pet-action", function(request, response){
@@ -107,6 +146,26 @@ app.post("/cadastrar-pet-action", function(request, response){
         response.render("message.ejs", {message: "Erro ao cadastrar pet: " + err});
     }
 });
+
+app.post("/cadastrarPet-action", function(request, response){
+    console.log(JSON.stringify(request.body));
+    var newPet = {
+        type: request.body.espec,
+        sex: request.body.sexo,
+        size: request.body.porte,
+        age: request.body.idadePet,
+        date: request.body.data,
+        address: request.body.endpet,
+    }
+    try{
+        //PetDAO.validate(newPet);
+        PetDAO.save(db, newPet);
+        response.render("message.ejs", {message: "Pet cadastrado com sucesso"});
+    }catch (err){
+        response.render("message.ejs", {message: "Erro ao cadastrar pet: " + err});
+    }
+});
+
 
 //configurando o driver do Mongo
 MongoClient.connect('mongodb://admin:takeapet@ds119091.mlab.com:19091/takeapet', function(err, database){
